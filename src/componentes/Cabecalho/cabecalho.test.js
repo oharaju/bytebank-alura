48333678bc014c777a881485da1003a9ba0ac7d6
Júlia Ohara <juliaohara.br@gmail.com>
2023-04-17 22:25:49 -0300

    feat: update teste

--- src/componentes/Cabecalho/cabecalho.test.js
+++ src/componentes/Cabecalho/cabecalho.test.js
@@ -1,4 +1,8 @@
-test('Primeiro Teste', () => {
-  const numero = 10;
-  expect(numero).toBe(10);
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('Renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuario).toBeInTheDocument();
 });
